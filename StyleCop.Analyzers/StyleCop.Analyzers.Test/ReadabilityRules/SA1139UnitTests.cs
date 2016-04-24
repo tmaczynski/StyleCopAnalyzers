8f8eca531314b4fb72fdcd5b3176627644f93354
Tomasz Maczyński <tmtimon@gmail.com>
2016-04-24 16:04:01 +0200

    SA1139UseLiteralsSuffixNotationInsteadOfCasting: first implementation of diagnostic and corresponding tests

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1139UnitTests.cs
@@ -0,0 +1,137 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+/* Contributor: Tomasz Maczyński */
+
+namespace StyleCop.Analyzers.Test.ReadabilityRules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using TestHelper;
+    using Xunit;
+    using Analyzers.ReadabilityRules;
+    using System.Threading;
+
+    public class SA1139UnitTests : CodeFixVerifier
+    {
+        /// <summary>
+        /// Verifies that using literal in a declaration of a class field does not produce diagnostic.
+        /// </summary>
+        /// <param name="literalType">The type which is checked.</param>
+        /// <param name="literalSuffix">The correpsonding literal's suffix.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [InlineData("long", "L")]
+        [InlineData("long", "l")]
+        [InlineData("ulong", "UL")]
+        [InlineData("ulong", "ul")]
+        [InlineData("ulong", "uL")]
+        [InlineData("ulong", "Ul")]
+        [InlineData("uint", "U")]
+        [InlineData("uint", "u")]
+        public async Task TestUsingLiteralsAsClassFieldsDoesNotProduceDiagnosticAsync(string literalType, string literalSuffix)
+        {
+            var testCode = $@"
+class ClassName
+{{
+    {literalType} x = 1{literalSuffix};
+}}
+";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that using casts in a declaration of a class field does produce diagnostic.
+        /// </summary>
+        /// <param name="literalType">The type which is checked.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [InlineData("long")]
+        [InlineData("ulong")]
+        [InlineData("uint")]
+        public async Task TestUsingCastsInFieldDeclarationProducesDiagnosticAsync(string literalType)
+        {
+            var testCode = $@"
+class ClassName
+{{
+    {literalType} x = ({literalType})1;
+}}
+";
+            DiagnosticResult[] expectedDiagnosticResult =
+            {
+                this.CSharpDiagnostic().WithLocation(4, 11 + literalType.Length)
+            };
+            await this.VerifyCSharpDiagnosticAsync(testCode, expectedDiagnosticResult, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that using literals in a method body does not produce diagnostic.
+        /// </summary>
+        /// <param name="literalSuffix">Literal's suffix.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [InlineData("L")]
+        [InlineData("l")]
+        [InlineData("UL")]
+        [InlineData("ul")]
+        [InlineData("uL")]
+        [InlineData("Ul")]
+        [InlineData("U")]
+        [InlineData("u")]
+        public async Task TestUsingLiteralsInMethodDoesNotProduceDiagnosticAsync(string literalSuffix)
+        {
+            var testCode = $@"
+class ClassName
+{{
+    public void Method()
+    {{
+        var x = 1{literalSuffix};
+    }}
+}}
+";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that using casts in a method body produces diagnostic.
+        /// </summary>
+        /// <param name="literalType">The type which is checked.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [InlineData("long")]
+        [InlineData("ulong")]
+        [InlineData("uint")]
+        public async Task TestUsingCastsInMethodProducesDiagnosticAsync(string literalType)
+        {
+            var testCode = $@"
+class ClassName
+{{
+    public void Method()
+    {{
+        var x = ({literalType})1;
+    }}
+}}
+";
+            DiagnosticResult[] expectedDiagnosticResult =
+            {
+                this.CSharpDiagnostic().WithLocation(6, 18)
+            };
+            await this.VerifyCSharpDiagnosticAsync(testCode, expectedDiagnosticResult, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            throw new NotImplementedException("CodeFixProvider is not available yet");
+        }
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1139UseLiteralsSuffixNotationInsteadOfCasting();
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1139UseLiteralsSuffixNotationInsteadOfCasting.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+/* Contributor: Tomasz Maczyński */
+
+namespace StyleCop.Analyzers.ReadabilityRules
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A cast is performed instead of using literal of a number. Use "U" suffix to create 32-bit unsigned integer literal, "L" for 64-bit integer literal and "UL" for 64-bit unsigned integer literal.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class SA1139UseLiteralsSuffixNotationInsteadOfCasting : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The ID for diagnostics produced by the <see cref="SA1139UseLiteralsSuffixNotationInsteadOfCasting"/> analyzer.
+        /// </summary>
+        public const string DiagnosticId = "SA1139";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(ReadabilityResources.SA1139Title), ReadabilityResources.ResourceManager, typeof(ReadabilityResources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(ReadabilityResources.SA1139MessageFormat), ReadabilityResources.ResourceManager, typeof(ReadabilityResources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(ReadabilityResources.SA1139Description), ReadabilityResources.ResourceManager, typeof(ReadabilityResources));
+        private static readonly string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1139.md";
+
+        private static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.ReadabilityRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
+
+        private static readonly Action<CompilationStartAnalysisContext> CompilationStartAction = HandleCompilationStart;
+        private static readonly Action<SyntaxNodeAnalysisContext> GenericNameAction = HandleGenericName;
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(CompilationStartAction);
+        }
+
+        private static void HandleCompilationStart(CompilationStartAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionHonorExclusions(GenericNameAction, SyntaxKind.CastExpression);
+        }
+
+        private static void HandleGenericName(SyntaxNodeAnalysisContext context)
+        {
+            CastExpressionSyntax castExpressionSyntax = (CastExpressionSyntax)context.Node;
+
+            var exprNodes = castExpressionSyntax.ChildNodes().ToList();
+            if (exprNodes.Count != 2)
+            {
+                return;
+            }
+
+            // TODO: handle cases when .NET type names are used e.g. Int64
+            // TODO: handle real literals https://msdn.microsoft.com/en-us/library/aa691085(v=vs.71).aspx
+            var castingToTypeSyntax = exprNodes[0] as PredefinedTypeSyntax;
+            var castedElementTypeSyntax = exprNodes[1] as LiteralExpressionSyntax;
+
+            if (castingToTypeSyntax == null || castedElementTypeSyntax == null)
+            {
+                return;
+            }
+
+            var syntaxKindKeyword = castingToTypeSyntax.Keyword.Kind();
+
+            if (syntaxKindKeyword != SyntaxKind.LongKeyword
+                && syntaxKindKeyword != SyntaxKind.ULongKeyword
+                && syntaxKindKeyword != SyntaxKind.UIntKeyword)
+            {
+                return;
+            }
+
+            var castedToken = castedElementTypeSyntax.Token;
+            if (!castedToken.IsKind(SyntaxKind.NumericLiteralToken))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, castingToTypeSyntax.GetLocation()));
+        }
+    }
+}
