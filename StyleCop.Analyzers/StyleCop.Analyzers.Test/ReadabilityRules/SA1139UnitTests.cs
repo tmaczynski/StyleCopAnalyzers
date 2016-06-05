01c5784b6ea0ee946ad9b31e23d28c88201f81b2
Tomasz Maczyński <t.maczynski@gmail.com>
2016-06-06 01:16:57 +0200

    delete unused using directive

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1139UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1139UnitTests.cs
@@ -8,7 +8,6 @@ namespace StyleCop.Analyzers.Test.ReadabilityRules
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.InteropServices;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
